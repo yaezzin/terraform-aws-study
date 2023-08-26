b40f6610dfb218b530ba7ac56a48986a28dca8c2
yaezzin <yaezzin@gmail.com>
2023-08-26 21:57:06 +0900

    provisioner

---
+++ provisioner/vars.tf
@@ -0,0 +1,21 @@
+variable "AWS_REGION" {
+  default = "ap-northeast-2"
+}
+
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
